c3c86c86cd844593f3a25e363c29a0e5afa0b7a1
Stephanie Koltun <stephkoltun@icloud.com>
2018-01-13 22:13:31 -0500

    changed links

--- 004-Maps/drawMap.js
+++ 004-Maps/drawMap.js
@@ -115,13 +115,13 @@ $(document).keypress(function(e) {
         // go to next map
         
         if (mapMode == 'neighborhood') {
-            window.location.href = "http://anothersideproject.co/known-and-strange/polygonDistribution.html";
+            window.location.href = "http://anothersideproject.co/known-and-strange/004/polygonDistribution.html";
         } else if (mapMode == 'polygons') {
-            window.location.href = "http://anothersideproject.co/known-and-strange/pointDistribution.html";
+            window.location.href = "http://anothersideproject.co/known-and-strange/004/pointDistribution.html";
         } else if (mapMode == 'points') {
-            window.location.href = "http://anothersideproject.co/known-and-strange/areaCategorization.html";
+            window.location.href = "http://anothersideproject.co/known-and-strange/004/areaCategorization.html";
         } else if (mapMode == 'areas') {
-            window.location.href = "http://anothersideproject.co/known-and-strange/neighborhood.html";
+            window.location.href = "http://anothersideproject.co/known-and-strange/004/index.html";
         }
         
     }
