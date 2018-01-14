e8375e4350ab7ce0985e0d170561c152cb8e4818
koltunburke-imac <stephkoltun@icloud.com>
2018-01-14 15:41:50 -0500

    successful shuffle

--- 005-TileSwap/tileswap.js
+++ 005-TileSwap/tileswap.js
@@ -2,29 +2,23 @@
 // initialize map
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3RlcGhrb2x0dW4iLCJhIjoiVXJJT19CQSJ9.kA3ZPQxKKHNngVAoXqtFzA';
 
+var startPoints = [
+    [-73.903207, 40.608448],
+    [-73.925492, 40.790892],
+    [-73.797266, 40.793105],
+
+];
+
+
+
 var map = new mapboxgl.Map({
     container: 'map',
     // satellite imagery styling
     style: 'mapbox://styles/mapbox/satellite-v9',
     //style: 'mapbox://styles/stephkoltun/cjcapx5je1wql2so4uigw0ovc',
     // set the start point of the map - needs to be long-lat (not lat-long)
-    center: [-73.903207, 40.608448],    // this should be a random point
+    center: startPoints[Math.floor(Math.random(startPoints.length))],    // this should be a random point
     zoom: 15,   // 10 - what scale
-});
-
-
-map.on('load', function () {
-    console.log("map is loaded");
-
-    swapPixels();
-
-    // need to debounce the render
-    map.on('dragend', function() {
-        map.on('render', function() {
-            swapPixels(); 
-        }) 
-    })
-
 });
 
 var nSubs = 4;
@@ -32,50 +26,124 @@ var imgWidth = 800;
 var subSize = imgWidth/nSubs;
 var totalSubs = nSubs*nSubs;
 
+//generate normal order
+var normalOrder = createOrder(nSubs);
+var newOrder = shuffleArray(normalOrder);
+console.log(normalOrder);
+console.log(newOrder);
+
+map.on('load', function () {
+    console.log("map is loaded");
+
+    // redraw the map
+    map.on('render', function() {
+        swapPixels();
+    })
+});
+
+
+
 function swapPixels() {
-    console.log("change");
 
     var canvasAll = document.getElementsByClassName("mapboxgl-canvas");
     var canvas = canvasAll[0];
 
-
     // get the 2D canvas
-    var canvas2D = document.getElementById("temp");
+    var canvas2D = document.getElementById("shuffle");
     var ctx2D = canvas2D.getContext("2d");
     // draw the webGL canvas as an image to the 2D canvas
     ctx2D.drawImage(canvas, 0, 0);
 
-    var ctxImageData = ctx2D.getImageData(400, 400, subSize, subSize);
-    var data =  ctxImageData.data;
-    var otherSub = ctx2D.getImageData(0, 0, subSize, subSize);
-    var otherData = otherSub.data;
+    for (var i = 0; i < newOrder.length; i = i+2) {
+        // set the first
+        var targetSub = newOrder[i];
+        var replaceSub = newOrder[i+1];
 
-    // reassign pixels elsewhere
-    for (var i = 0; i < data.length; i += 4) {
-      data[i]     = otherData[i];     // red
-      data[i + 1] = otherData[i + 1]; // green
-      data[i + 2] = otherData[i + 2]; // blue
+        var targetX = targetSub.x*subSize;
+        var targetY = targetSub.y*subSize;
+        // get the target subdivision
+        var targetImage = ctx2D.getImageData(targetX, targetY, subSize, subSize);
+        var targetData = targetImage.data;
+
+        // duplicate this data
+        var duplicateTarget = targetData.slice();
+
+        // use the next one in the sequence
+        
+        var replaceX = replaceSub.x*subSize;
+        var replaceY = replaceSub.y*subSize;
+
+        var replaceImage = ctx2D.getImageData(replaceX, replaceY, subSize, subSize);
+        var replaceData = replaceImage.data;
+
+        // swap arrays
+        for (var k = 0; k < targetData.length; k += 4) {
+          targetData[k]     = replaceData[k];     // red
+          replaceData[k]    = duplicateTarget[k];
+          //eplaceData[k]    = 255;
+
+          targetData[k+1]   = replaceData[k+1]; // green
+          replaceData[k+1]  = duplicateTarget[k+1];
+
+          targetData[k+2]   = replaceData[k+2]; // blue
+          replaceData[k+2]  = duplicateTarget[k+2];
+        }
+
+        ctx2D.putImageData(targetImage, targetX, targetY);
+        ctx2D.putImageData(replaceImage, replaceX, replaceY);
+
     }
 
-    ctx2D.putImageData(ctxImageData, 400, 400);
 
-    // for (var subRow = 1; subRow < nSubs; subRow++) {
-    //     for (var subCol = 1; subCol < totalSubs; subCol++) {
-    //         var xPos = subRow*subSize;
-    //         var yPos = subRow*subSize;
 
-    //         var ctxImageData = ctx2D.getImageData(xPos, yPos, subSize, subSize);
-    //         var data =  ctxImageData.data;
+    // var ctxImageData = ctx2D.getImageData(400, 400, subSize, subSize);
+    // var data =  ctxImageData.data;
+    // var otherSub = ctx2D.getImageData(0, 0, subSize, subSize);
+    // var otherData = otherSub.data;
 
-    //         var otherSub = ctx2D.getImageData(0, 0, subSize, subSize);
-    //         var otherdata = otherSub.data;
-
-    //         ctx2D.putImageData(otherSub, xPos, yPos);
-    //     }
+    // // reassign pixels elsewhere
+    // for (var i = 0; i < data.length; i += 4) {
+    //   data[i]     = otherData[i];     // red
+    //   data[i + 1] = otherData[i + 1]; // green
+    //   data[i + 2] = otherData[i + 2]; // blue
     // }
 
-
+    // ctx2D.putImageData(ctxImageData, 400, 400);
     
+}
+
+function createOrder(subs) {
+    var xyPairs = [];
+    for (var x = 0; x < subs; x++) {
+        for (var y = 0; y < subs; y++) {
+            var pair = {
+                'x': x,
+                'y': y
+            }
+            xyPairs.push(pair);
+        }
+    }
+    return xyPairs;
+}
+
+function shuffleArray (originalArray) {
+
+    var array = [].concat(originalArray);
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+    // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
 }
 
 
