51bd6e6896150cf503628f22297eed775c864b8e
koltunburke-imac <stephkoltun@icloud.com>
2018-02-12 12:27:11 -0500

    intial commit for day 34

---
+++ 034-Routine-Grid/gatherfiles.py
@@ -0,0 +1,29 @@
+from os import listdir
+from os.path import isfile, join
+
+classname = 'lazy'
+#mypath = 'img/monday/'
+#mypath = 'img/tuesday/'
+#mypath = 'img/wednesday/'
+mypath = 'img/thursday/'
+
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+totalString = ''
+
+for index, filename in enumerate(onlyfiles):
+    # a photo every five minutes
+    if (index % 10 == 0):
+        # create HTML structure
+        htmlbase = "<img class='" + classname + "' data-original='" + mypath + filename + "'>\n"
+        totalString += htmlbase
+
+#f = open('monday.html','w')
+#f = open('tuesday.html','w')
+#f = open('wednesday.html','w')
+f = open('thursday.html','w')
+f.write(totalString)
+f.close()
+
+print ("done")
+#print (totalString)
