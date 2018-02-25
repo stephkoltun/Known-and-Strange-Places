5c2c7382e1361381a6f81c55b3456b0c673741b0
koltunburke-imac <stephkoltun@icloud.com>
2018-02-25 09:24:17 -0500

    updated 047 but not functional on web

--- 047-Body-Context/context.js
+++ 047-Body-Context/context.js
@@ -1,31 +1,63 @@
 var colorVid;
 var depthVid;
-var threshold = 100;
+var threshold = 15;
+var depthLoad = false;
+var colorLoad = false;
+
+function preload() {
+  depthVid = createVideo(['mov/depth.mp4'], depthLoaded);
+  colorVid = createVideo(['mov/color.mp4'], colorLoaded);
+}
+
 function setup() {
   createCanvas(423, 344);
-  colorVid = createVideo(['img/color.mov']);
+}
+
+function depthLoaded() {
+  console.log("depth loaded");
+  depthLoad = true;
+  depthVid.hide();
+
+  if (colorLoad) {
+    colorVid.loop();
+    depthVid.loop();
+  }
+}
+
+function colorLoaded() {
+  console.log("color loaded");
+  colorLoad = true;
   colorVid.hide();
 
-  depthVid = createVideo(['img/depth.mov']);
-  depthVid.hide();
+  if (depthLoad) {
+    colorVid.loop();
+    depthVid.loop();
+  }
+}
+
+function keyPressed() {
+  threshold++;
+  console.log(threshold);
 }
 
 function draw() {
-  image(colorVid, 0, 0);
+  background(0);
+  if (colorLoad && depthLoad) {
+    image(colorVid, 0, 0);
 
-  depthVid.loadPixels();
-  var depthPix = depthVid.pixels;
+    depthVid.loadPixels();
+    var depthPix = depthVid.pixels;
 
-  for (var x = 0; x < depthVid.width; x++ ) {
-    for (var y = 0; y < depthVid.height; y++ ) {
-      var index = (x + y*displaySize) * 4;
+    for (var x = 0; x < depthVid.width; x++ ) {
+      for (var y = 0; y < depthVid.height; y++ ) {
+        var index = (x + y*depthVid.width) * 4;
+        var depthVal = depthPix[index];
 
-      var depthVal = depthPix[index];
-
-      if (depthVal <= 100) {
-        noStroke();
-        fill();
-        rect(x,y,1,1);
+        if (depthVal >= threshold) {
+          noStroke();
+          fill(0);
+          rect(x,y,1,1);
+        }
       }
     }
   }
