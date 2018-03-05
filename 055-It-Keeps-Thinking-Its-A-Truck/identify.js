a22d736d177a2932129eba9cea3ec98a3ffdfb53
koltunburke-imac <stephkoltun@icloud.com>
2018-03-05 15:21:16 -0500

    updated to cycle through inidividual images one at a time

--- 055-It-Keeps-Thinking-Its-A-Truck/identify.js
+++ 055-It-Keeps-Thinking-Its-A-Truck/identify.js
@@ -1,57 +1,13 @@
-var imgArray = [];
-
 function preload(){
   console.log(detects.length);
-
-  for (var im = 0; im < 100; im++) {
-    //for (var im = 0; im < imageFiles.length; im++) {
-    var path = imageFiles[im];
-    var temp = loadImage(path);
-    imgArray.push(temp);
-  }
 }
 
-var acceptableImages = 0;
 function setup() {
   var cnv = createCanvas(800,640);
-  cnv.parent("wrapper");
-
-  // for (var i = 0; i < imgArray.length; i++) {
-  //   var prevWidth = 0;
-  //   //image(imgArray[i],0,0);
-  //   var thisImg = imgArray[i];
-  //   var imgOffset = 150*acceptableImages;
-  //   var horizontalOffset = 10;
-  //
-  //   var predictions = detects[i].predictions;
-  //
-  //   if (predictions.length != 0) {
-  //     acceptableImages++;
-  //     for (var p = 0; p < predictions.length; p++) {
-  //       var box = predictions[p].box;
-  //       var bWidth = box.right - box.left;
-  //       var bHeight = box.bot - box.top;
-  //
-  //       // noFill();
-  //       // stroke(0,255,255);
-  //       // rect(box.left,box.top,bWidth,bHeight);
-  //
-  //       var detected = thisImg.get(box.left,box.top,bWidth,bHeight);
-  //
-  //       if (predictions[p].class == 'person') {
-  //         image(detected, box.left, box.top);
-  //         //tint(255, 60);
-  //       }
-  //
-  //
-  //
-  //       //image(detected, 10+prevWidth, 10+imgOffset);
-  //       prevWidth = prevWidth + bWidth + horizontalOffset;
-  //     }
-  //   }
-  // }
+  cnv.parent("wrapper")
 }
 
+var curImageObj;
 var curImage = -1;
 var classList = [];
 var curClassIndex = -1;
@@ -67,7 +23,12 @@ function draw() {
       curClassIndex++;
     } else {
       // go to next image
-      curImage++;
+      if (curImage < imageFiles.length-1) {
+        curImage++;
+      } else {
+        curImage = 0;
+      }
+
       curClassIndex = 0;
       classList = [];
 
@@ -82,31 +43,33 @@ function draw() {
     showClasses();
     fill(0);
     if (classList.length > 0) {
-      text
-      text(classList[curClassIndex], 20, 20);
+      textAlign(CENTER,CENTER);
+      textFont("Karla");
+      textSize(32);
+      text(classList[curClassIndex], width/2, 50);
     }
   }
 }
 
 
 function showClasses() {
-  var thisImg = imgArray[curImage];
-  var predictions = detects[curImage].predictions;
-  var targetClass = classList[curClassIndex];
+  var path = imageFiles[curImage];
+  loadImage(path, function(thisImg) {
+    var predictions = detects[curImage].predictions;
+    var targetClass = classList[curClassIndex];
 
-  if (predictions.length > 0) {
-    for (var p = 0; p < predictions.length; p++) {
-      var box = predictions[p].box;
-      var bWidth = box.right - box.left;
-      var bHeight = box.bot - box.top;
+    if (predictions.length > 0) {
+      for (var p = 0; p < predictions.length; p++) {
+        var box = predictions[p].box;
+        var bWidth = box.right - box.left;
+        var bHeight = box.bot - box.top;
 
-      var detected = thisImg.get(box.left,box.top,bWidth,bHeight);
+        var detected = thisImg.get(box.left,box.top,bWidth,bHeight);
 
-      if (predictions[p].class == targetClass) {
-        image(detected, box.left, box.top);
+        if (predictions[p].class == targetClass) {
+          image(detected, box.left, box.top);
+        }
       }
     }
-  }
-
-
+  });
 }
---
+++ 055-It-Keeps-Thinking-Its-A-Truck/individual-identify.js
@@ -0,0 +1,91 @@
+function preload(){
+  console.log(detects.length);
+}
+
+function setup() {
+  var cnv = createCanvas(800,640);
+  cnv.parent("wrapper")
+}
+
+var curImage = 0;
+var predTotal;
+var predIndex = 0;
+
+function draw() {
+  if (frameCount % 40 == 0) {
+    noStroke();
+    fill(255,250);
+    rect(0,0,width,height);
+
+    getCounts();
+  }
+}
+
+function getCounts() {
+  var predictions = detects[curImage].predictions;
+  predTotal = predictions.length;
+
+  if (predTotal == 0) {
+    curImage++;
+    getCounts();
+    predIndex = 0;
+    predTotal = 0;
+  } else {
+    showClasses();
+  }
+}
+
+
+function showClasses() {
+  var predictions = detects[curImage].predictions;
+
+  var path = imageFiles[curImage];
+  loadImage(path, function(thisImg) {
+    var box = predictions[predIndex].box;
+    var bWidth = box.right - box.left;
+    var bHeight = box.bot - box.top;
+    var detected = thisImg.get(box.left,box.top,bWidth,bHeight);
+
+    // var scaleWidth;
+    // var scaleHeight;
+    // if (bWidth > bHeight) {
+    //   scaleWidth = width/2;
+    //   scaleHeight = (bHeight*scaleWidth)/bWidth;
+    // } else {
+    //   scaleHeight = height/2;
+    //   scaleWidth = (scaleHeight*bWidth)/bHeight;
+    // }
+    //
+    // var xPos = width/2 - scaleWidth/2;
+    // var yPos = height/2 - scaleHeight/2;
+    // image(detected, xPos, yPos, scaleWidth, scaleHeight);
+
+    var xPos = width/2 - bWidth*3/2;
+    var yPos = height/2 - bHeight*3/2;
+    image(detected, xPos, yPos, bWidth*3, bHeight*3);
+
+    fill(0);
+    textFont("Karla");
+    textSize(48);
+
+    textAlign(LEFT,CENTER);
+    text("This is a " + predictions[predIndex].class, 0, height-60);
+    // textAlign(CENTER,CENTER);
+    // text(predictions[predIndex].class, width/2, height-60);
+
+    if (predIndex < predTotal-1) {
+      predIndex++;
+    } else {
+      predIndex = 0;
+      predTotal = 0;
+      // go to next image
+      if (curImage < imageFiles.length-1) {
+        curImage++;
+      } else {
+        curImage = 0;
+      }
+    }
+  });
+
+
+}
