618046252179cebd33d513df67e975a0ddad51b2
koltunburke-imac <stephkoltun@icloud.com>
2018-03-07 09:42:21 -0500

    added 067

---
+++ 067-Different-Similar-Contexts/allTiles.js
@@ -0,0 +1,128 @@
+var showSimilar;
+var allTiles = [
+  {
+    id: 1,
+    geoCenter: tileOne.features[0].geometry.coordinates,
+    similarPlaces: tileOne.features,
+  },
+  {
+    id: 2,
+    geoCenter: tileTwo.features[0].geometry.coordinates,
+    similarPlaces: tileTwo.features,
+  },
+  {
+    id: 3,
+    geoCenter: tileThree.features[0].geometry.coordinates,
+    similarPlaces: tileThree.features,
+  },
+  {
+    id: 4,
+    geoCenter: tileFour.features[0].geometry.coordinates,
+    similarPlaces: tileFour.features,
+  },
+  {
+    id: 5,
+    geoCenter: tileFive.features[0].geometry.coordinates,
+    similarPlaces: tileFive.features,
+  },
+  {
+    id: 6,
+    geoCenter: tileSix.features[0].geometry.coordinates,
+    similarPlaces: tileSix.features,
+  },
+  {
+    id: 7,
+    geoCenter: tileSeven.features[0].geometry.coordinates,
+    similarPlaces: tileSeven.features,
+  },
+  {
+    id: 8,
+    geoCenter: tileEight.features[0].geometry.coordinates,
+    similarPlaces: tileEight.features,
+  },
+  {
+    id: 9,
+    geoCenter: tileNine.features[0].geometry.coordinates,
+    similarPlaces: tileNine.features,
+  },
+  {
+    id: 10,
+    geoCenter: tileTen.features[0].geometry.coordinates,
+    similarPlaces: tileTen.features,
+  },
+  {
+    id: 11,
+    geoCenter: tileEleven.features[0].geometry.coordinates,
+    similarPlaces: tileEleven.features,
+  },
+  {
+    id: 12,
+    geoCenter: tileTwelve.features[0].geometry.coordinates,
+    similarPlaces: tileTwelve.features,
+  },
+  {
+    id: 13,
+    geoCenter: tileThirteen.features[0].geometry.coordinates,
+    similarPlaces: tileThirteen.features,
+  },
+  {
+    id: 14,
+    geoCenter: tileFourteen.features[0].geometry.coordinates,
+    similarPlaces: tileFourteen.features,
+  },
+  {
+    id: 15,
+    geoCenter: tileFifteen.features[0].geometry.coordinates,
+    similarPlaces: tileFifteen.features,
+  },
+  {
+    id: 16,
+    geoCenter: tileSixteen.features[0].geometry.coordinates,
+    similarPlaces: tileSixteen.features,
+  },
+  {
+    id: 17,
+    geoCenter: tileSeventeen.features[0].geometry.coordinates,
+    similarPlaces: tileSeventeen.features,
+  },
+  {
+    id: 18,
+    geoCenter: tileEighteen.features[0].geometry.coordinates,
+    similarPlaces: tileEighteen.features,
+  },
+  {
+    id: 19,
+    geoCenter: tileNineteen.features[0].geometry.coordinates,
+    similarPlaces: tileNineteen.features,
+  },
+  {
+    id: 20,
+    geoCenter: tileTwenty.features[0].geometry.coordinates,
+    similarPlaces: tileTwenty.features,
+  },
+  {
+    id: 21,
+    geoCenter: tileTwentyOne.features[0].geometry.coordinates,
+    similarPlaces: tileTwentyOne.features,
+  },
+  {
+    id: 22,
+    geoCenter: tileTwentyTwo.features[0].geometry.coordinates,
+    similarPlaces: tileTwentyTwo.features,
+  },
+  {
+    id: 23,
+    geoCenter: tileTwentyThree.features[0].geometry.coordinates,
+    similarPlaces: tileTwentyThree.features,
+  },
+  {
+    id: 24,
+    geoCenter: tileTwentyFour.features[0].geometry.coordinates,
+    similarPlaces: tileTwentyFour.features,
+  },
+  {
+    id: 25,
+    geoCenter: tileTwentyFive.features[0].geometry.coordinates,
+    similarPlaces: tileTwentyFive.features,
+  }
+]
