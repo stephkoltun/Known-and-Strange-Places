52146165c69d7b57e5dc3027c20087cf00ad15c6
koltunburke <pburke@Patricks-MBP-4.fios-router.home>
2018-10-08 10:57:05 -0400

    043 revision to single frame with section lines at mouse position

--- 043-Thinly-Sliced/sliced.js
+++ 043-Thinly-Sliced/sliced.js
@@ -4,6 +4,8 @@ var depthImage;
 var depth = [];
 var skip = 1;
 
+var mode = "single" // "single","double"
+
 $("button").click(function() {
   if ($(this).hasClass("active")) {
     $(this).removeClass("active");
@@ -40,45 +42,83 @@ function setup() {
 }
 
 function draw() {
-  image(depthImage,0,0,1024,848);
-  image(depthImage,1024,0,1024,848);
 
-  image(rgbImage, 0,0,1024,848);
+  if (mode == "single") {
+    image(depthImage,512,0,1024,848);
 
-  strokeWeight(3);
-  noFill();
-
-  var xMPos = constrain(mouseX,0,1024);
-  var x = floor(map(xMPos,0,1024,0,511));
-  // draw the line at which the section is cut (MIDDLE)
-  if (showHorizontal) {
-    stroke("rgb(180, 180, 0)");
-    line(x*2, 0, x*2, height);
-    noStroke();
-    fill("rgba(180, 180, 0, 0.25)")
-    rect(0,0,x*2, height);
+    strokeWeight(3);
     noFill();
-    stroke("rgb(255, 255, 0)");
-    drawSection(x, "X");
+
+    var xMPos = constrain(mouseX,512,1536);
+    var x = floor(map(xMPos,512,1536,0,511));
+    // draw the line at which the section is cut (MIDDLE)
+    if (showHorizontal) {
+      // stroke("rgb(180, 180, 0)");
+      // line(x*2, 0, x*2, height);
+      noStroke();
+      fill("rgba(180, 180, 0, 0.25)")
+      rect(512,0,x*2, height);
+      noFill();
+      stroke("rgb(255, 255, 0)");
+      drawSection(x, "X", 512);
+    }
+
+    if (showVertical) {
+      var yMPos = constrain(mouseY,0,848);
+      var y = floor(map(yMPos,0,848,0,424));
+      // stroke("rgb(0, 200, 200)");
+      // line(0, y*2, width/2, y*2);
+      noStroke();
+      fill("rgba(0, 200, 200, 0.25)")
+      rect(512,0,width/2, y*2);
+      noFill();
+      stroke("rgb(0, 255, 255)");
+      drawSection(y, "Y", 512);
+    }
+
   }
 
-  if (showVertical) {
-    var yMPos = constrain(mouseY,0,848);
-    var y = floor(map(yMPos,0,848,0,424));
-    stroke("rgb(0, 200, 200)");
-    line(0, y*2, width/2, y*2);
-    noStroke();
-    fill("rgba(0, 200, 200, 0.25)")
-    rect(0,0,width/2, y*2);
+  if (mode == "double") {
+    image(depthImage,0,0,1024,848);
+    image(depthImage,1024,0,1024,848);
+
+    image(rgbImage, 0,0,1024,848);
+
+    strokeWeight(3);
     noFill();
-    stroke("rgb(0, 255, 255)");
-    drawSection(y, "Y");
+
+    var xMPos = constrain(mouseX,0,1024);
+    var x = floor(map(xMPos,0,1024,0,511));
+    // draw the line at which the section is cut (MIDDLE)
+    if (showHorizontal) {
+      stroke("rgb(180, 180, 0)");
+      line(x*2, 0, x*2, height);
+      noStroke();
+      fill("rgba(180, 180, 0, 0.25)")
+      rect(0,0,x*2, height);
+      noFill();
+      stroke("rgb(255, 255, 0)");
+      drawSection(x, "X", 1023);
+    }
+
+    if (showVertical) {
+      var yMPos = constrain(mouseY,0,848);
+      var y = floor(map(yMPos,0,848,0,424));
+      stroke("rgb(0, 200, 200)");
+      line(0, y*2, width/2, y*2);
+      noStroke();
+      fill("rgba(0, 200, 200, 0.25)")
+      rect(0,0,width/2, y*2);
+      noFill();
+      stroke("rgb(0, 255, 255)");
+      drawSection(y, "Y", 1023);
+    }
   }
 
 }
 
 
-function drawSection(_v, _mode) {
+function drawSection(_v, _mode, _xOffset) {
   // Get the raw depth as array of integers
 
   beginShape();
@@ -86,18 +126,18 @@ function drawSection(_v, _mode) {
     for (var y = 0; y < 424; y += skip) {
       var offset = _v + y * 512;
       var depthVal = depth[offset];
-      var xPos = map(depthVal, 0, 255, 1024, 0);
+      var xPos = map(depthVal, 0, 255, 400, 0);  // how wide should our section be?
       if (depthVal != 0) {
-        vertex(xPos+1023, y*2);
+        vertex(xPos+mouseX-700+_xOffset, y*2);
       }
     }
   } else if ( _mode == "Y") {
     for (var x = 0; x < 512; x += skip) {
       var offset = x + _v * 512;
       var depthVal = depth[offset];
-      var yPos = map(depthVal, 0, 255, 848, 0);
+      var yPos = map(depthVal, 0, 255, 400, 0); // how tall could our section be?
       if (depthVal != 0) {
-        vertex(x*2+1023, yPos);
+        vertex(x*2+_xOffset, yPos+mouseY-200);
       }
     }
   }
