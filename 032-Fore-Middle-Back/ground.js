d65d42a4e68c3ddec205694973e55d332154e871
koltunburke-imac <stephkoltun@icloud.com>
2020-03-14 14:09:45 -0400

    032 size and interaction

--- 032-Fore-Middle-Back/ground.js
+++ 032-Fore-Middle-Back/ground.js
@@ -1,41 +1,42 @@
-
-$("#desc").delay(3000).fadeOut(1000);
-
 var curImg = '2';
 
+// frontpix, midpix, backpix
+// imgsize = 2204; fixed pixel array size
+
+// <img src="img/foreground.png" class="hid" id="1">
+// <img src="img/middleground.png" class="vis" id="2">
+// <img src="img/background.png" class="hid" id="3">
+
 $("body").mousemove(function(event) {
-    var windowWidth = $(window).width();
-    if (event.pageX < windowWidth/3 && curImg != '1') {
-        // change img
-				if (curImg == '2') {
-					$('#2').addClass("hid").removeClass("vis");
-				} else if (curImg == '3') {
-					$('#3').addClass("hid").removeClass("vis");
-				}
 
-				curImg = '1';
-        $('#1').addClass("vis").removeClass("hid");
+  var pixPos = event.pageX + event.pageY*2204;
+
+  if (frontpix[pixPos] === 1 && curImg != '1') {
+    if (curImg == '2') {
+      $('#2').addClass("hid").removeClass("vis");
+    } else if (curImg == '3') {
+      $('#3').addClass("hid").removeClass("vis");
     }
 
-    if ((event.pageX > windowWidth/3) && (event.pageX < (windowWidth/3)*2) && (curImg != '2')) {
-			// change img
-			if (curImg == '1') {
-				$('#1').addClass("hid").removeClass("vis");
-			} else if (curImg == '3') {
-				$('#3').addClass("hid").removeClass("vis");
-			}
-			curImg = '2';
-			$('#2').addClass("vis").removeClass("hid");
+    curImg = '1';
+    $('#1').addClass("vis").removeClass("hid");
+  } else if (midpix[pixPos] === 1 && (curImg != '2')) {
+    // change img
+    if (curImg == '1') {
+      $('#1').addClass("hid").removeClass("vis");
+    } else if (curImg == '3') {
+      $('#3').addClass("hid").removeClass("vis");
     }
-
-		if ((event.pageX > (windowWidth/3)*2) && (curImg != '3')) {
-			// change img
-			if (curImg == '1') {
-				$('#1').addClass("hid").removeClass("vis");
-			} else if (curImg == '2') {
-				$('#2').addClass("hid").removeClass("vis");
-			}
-			curImg = '3';
-			$('#3').addClass("vis").removeClass("hid");
+    curImg = '2';
+    $('#2').addClass("vis").removeClass("hid");
+  } else if (backpix[pixPos] === 1 && (curImg != '3')) {
+    // change img
+    if (curImg == '1') {
+      $('#1').addClass("hid").removeClass("vis");
+    } else if (curImg == '2') {
+      $('#2').addClass("hid").removeClass("vis");
     }
+    curImg = '3';
+    $('#3').addClass("vis").removeClass("hid");
+  }
 });
