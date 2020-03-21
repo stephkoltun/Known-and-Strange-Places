184454682491a6b059b0df8ae14a09019eabae50
koltunburke-imac <stephkoltun@icloud.com>
2020-03-21 12:58:39 -0400

    038 sizing for 1920 1080

--- 038-Frankenstein/frankenstein.js
+++ 038-Frankenstein/frankenstein.js
@@ -4,10 +4,12 @@ mapboxgl.accessToken = key;
 
 var smallPossibles = [1,2,3,4,6,7,8,9,11,12,13,14,16,17,18,19];
 var randomSmall = Math.floor(Math.random() * Math.floor(smallPossibles.length));
+//var randomSmall = 1;
 var smallStart = [randomSmall, randomSmall+1, randomSmall+5, randomSmall+6];
 
 var largePossibles = [1,2,6,7];
-var randomLarge = Math.floor(Math.random() * Math.floor(largePossibles.length));
+//var randomLarge = Math.floor(Math.random() * Math.floor(largePossibles.length));
+var randomLarge = 1;
 var largeStart = [randomLarge, randomLarge+1, randomLarge+2,randomLarge+3,randomLarge+5, randomLarge+6, randomLarge+7,randomLarge+8,randomLarge+10, randomLarge+11, randomLarge+12,randomLarge+13,randomLarge+15, randomLarge+16, randomLarge+17,randomLarge+18];
 
 
@@ -15,8 +17,8 @@ var configSmallest = {
   cols: 2,
   rows: 2,
   totalTiles: 4,
-  dim: 256,
-  zoom: 18,
+  dim: 360,
+  zoom: 18.49,
   divIds: ['tile1', 'tile2', 'tile3', 'tile4'],
   includeTiles: smallStart,
   // divIds: ['tile1', 'tile2', 'tile3', 'tile4', 'tile5', 'tile6', 'tile7', 'tile8', 'tile9', 'tile10', 'tile11', 'tile12', 'tile13', 'tile14', 'tile15', 'tile16'],
@@ -29,8 +31,8 @@ var configLargest = {
   cols: 4,
   rows: 4,
   totalTiles: 16,
-  dim: 64,
-  zoom: 16,
+  dim: 180,
+  zoom: 17.5,
   divIds: ['tile1', 'tile2', 'tile3', 'tile4', 'tile5', 'tile6', 'tile7', 'tile8', 'tile9', 'tile10', 'tile11', 'tile12', 'tile13', 'tile14', 'tile15', 'tile16'],
   includeTiles: largeStart,
 }
